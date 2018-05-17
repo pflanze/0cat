b05f1e214d6bbc7ecfbed9fecca675f378739e48
Christian Jaeger <ch@christianjaeger.ch>
2018-05-17 17:43:17 +0100

    create 0cat.c, using splice
    Works, but only does it in chunks of 8192 bytes. That foils the aim I
    had, that the receiving process can call fstat on stdin and know the
    full file length.

---
+++ 0cat.c
@@ -0,0 +1,69 @@
+/* Copyright by Christian Jaeger, ch@christianjaeger.ch
+   Published under the terms of the GNU GPL */
+
+#define _GNU_SOURCE
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+void usage () {
+    fprintf(stderr,
+	    "usage: 0cat filename\n"
+	    "  Opens filename then splices its filehandle into stdout.\n"
+	    "  Afterwards exits with exit code 0 if successful.\n");
+    exit(1);
+}
+
+
+int main (int argn, char**argv) {
+    if (argn!= 2) {
+	usage ();
+    }
+    int fd= open(argv[1], O_RDONLY);
+    if (fd < 0) {
+	fprintf(stderr, "0cat: open: %s: %s\n",
+		argv[1],
+		strerror(errno));
+	exit(1);
+    }
+    struct stat st;
+    if (fstat(fd, &st) < 0) {
+	fprintf(stderr, "0cat: fstat for: %s: %s\n",
+		argv[1],
+		strerror(errno));
+	exit(1);
+    }
+    size_t len= st.st_size;
+    while (len) {
+	ssize_t nspliced=
+	    splice(fd, NULL,
+		   1, NULL,
+		   len,
+		   0);
+	if (nspliced < 0) {
+	    fprintf(stderr, "0cat: splice for: %s: %s\n",
+		    argv[1],
+		    strerror(errno));
+	    exit(1);
+	}
+	len-= nspliced;
+	if (len) { fprintf(stderr,"0cat: %i bytes to go\n", len); }
+    }
+    if (close(1)<0) {
+	fprintf(stderr, "0cat: close stdout: %s\n",
+		strerror(errno));
+	exit(1);
+    }
+    if (close(fd)<0) {
+	fprintf(stderr, "0cat: close: %s: %s\n",
+		argv[1],
+		strerror(errno));
+	exit(1);
+    }
+    return 0;
+}
---
+++ Makefile
@@ -0,0 +1,3 @@
+0cat: 0cat.c
+	gcc -Wall -o 0cat 0cat.c
+
